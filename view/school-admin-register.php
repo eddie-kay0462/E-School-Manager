68b90627c46c7a53251112534dbaf036220cdb88
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-14 18:22:40 +0000

    Made the nav sticky

--- view/school-admin-register.php
+++ view/school-admin-register.php
@@ -61,7 +61,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
         <div class="container">
             <a class="navbar-brand" href="../index.php">Josephus Memorial School</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
