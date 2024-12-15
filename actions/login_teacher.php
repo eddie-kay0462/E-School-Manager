068ba026e358bd3c6c5aa1fc3104c1c980ca420c
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-15 17:55:52 +0000

    Added session management

--- actions/login_teacher.php
+++ actions/login_teacher.php
@@ -1,10 +1,10 @@
 <?php
 include '../db/config2.php';
+session_start();
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
 
 if (isset($_POST['teacher_id']) && isset($_POST['email']) && isset($_POST['password'])) {
     // Sanitize inputs
